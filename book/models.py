f27cda361cf9eec3ce99db3b415ea2320c1e5836
Oleksandra <mremarka314@gmail.com>
2023-04-11 19:28:01 +0300

    book series model added

--- book/models.py
+++ book/models.py
@@ -2,12 +2,36 @@ from django.db import models
 from author.models import Author
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    authors = models.ManyToManyField(Author, related_name="books")
+class BookSeries(models.Model):
+    name = models.CharField(max_length=200)
 
     objects = models.Manager()
 
+    @property
+    def books_count(self):
+        return self.books.all().count()
+
+    class Meta:
+        default_related_name = "book_series"
+        verbose_name_plural = "book series"
+
+    def __str__(self):
+        return self.name
+
+    def display_books(self):
+        return ', '.join(book.title for book in self.books.all())
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author)
+    series = models.ForeignKey(BookSeries, on_delete=models.DO_NOTHING, null=True, blank=True)
+    order_in_series = models.IntegerField(default=1)
+    objects = models.Manager()
+
+    class Meta:
+        default_related_name = "books"
+        ordering = ['series', 'order_in_series', 'title']
     def __str__(self):
         return self.title
 
@@ -15,3 +39,5 @@ class Book(models.Model):
         return ', '.join(a.fullname for a in self.authors.all()[:3])
 
     display_authors.short_description = "authors"
+
+# add book instances (physical books in a library)
