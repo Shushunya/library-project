97e0a95e3ecf7b15e5b522cdd2f633090cce2b47
Oleksandra <mremarka314@gmail.com>
2023-04-11 16:20:22 +0300

    author&book routes

--- library/urls.py
+++ library/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
+    path('authors/', include('author.urls')),
+    path('books/', include('book.urls')),
 ]
