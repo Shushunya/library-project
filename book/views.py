f0dc8d264b9be09d51e8788d8c5086452928518c
Oleksandra <mremarka314@gmail.com>
2023-04-12 16:22:03 +0300

    book series add to context & display info

--- book/views.py
+++ book/views.py
@@ -23,13 +23,23 @@ class BookDetail(DetailView):
     context_object_name = 'book'
 
     def get_context_data(self, **kwargs):
+        from book.models import BookSeries
         context = super().get_context_data(**kwargs)
         authors = [{
             'id': author.id,
             'fullname': author.fullname
         } for author in kwargs['object'].authors.all()]
+        series_queryset = kwargs['object'].series
+        if series_queryset is not None:
+            form = BookSeries.SeriesName(
+                    value=series_queryset.form_name).name.lower()
+            series = {
+                'id': series_queryset.id,
+                'title': f"{series_queryset.name} {form}",
+                'book_number': kwargs['object'].order_in_series
+            }
+            context['series'] = series
         context['authors'] = authors
-
         return context
 
 
